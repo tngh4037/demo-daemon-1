a84da6ee0ed52413effbc4a25909a38d3f8f8338
tngh4037 <tngh4037@gmail.com>
2026-04-24 09:23:04 +0900

    comment: 주석 추가 (todo)

--- src/main/java/process/daemon/goods/GoodsFetchTask.java
+++ src/main/java/process/daemon/goods/GoodsFetchTask.java
@@ -25,15 +25,24 @@ public class GoodsFetchTask implements Runnable {
         try {
             int count = 0;
 
-            while (!Thread.currentThread().isInterrupted()) {
+            // while (!Thread.currentThread().isInterrupted()) {
+            while (!Thread.interrupted()) { // 자신이 인터럽트 상태인지 체크 및 인터럽트 상태(true)라면 다시 정상으로 상태 변환(false)
                 count++;
 
                 // 상품 조회 (ex. DB list fetch)
+                /*
+                List<GoodsDto> fetchTarget = repository.getGoodsList();
+                if (fetchTarget.empty()) {
+                    // 계속해서 DB Fetch 하지 않음
+                    Thread.yield(); or Thread.sleep(10);
+                    continue;
+                }
+                */
+
+                // 반복문을 통한 fetchTarget 요소 큐 삽입
                 Integer goodsNo = count;
                 log.info("Fetch goodsNo: {}", goodsNo);
                 queue.put(goodsNo); // blocking
-
-                Thread.sleep(100);
             }
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
@@ -41,3 +50,6 @@ public class GoodsFetchTask implements Runnable {
         }
     }
 }
+
+// TODO :: try-catch는 blocking 메서드가 있는곳으로만 한정할지 고민.
+//   ㄴ 스레드와 생명주기2 - 19페이지 ( 프린터 예제3 - 인터럽트 코드 개선 ) 보기
